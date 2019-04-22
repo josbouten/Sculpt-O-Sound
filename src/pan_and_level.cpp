ec72aa891a456722de70ea8b2294574688a2232d
Jos Bouten <josbouten@gmail.com>
2019-04-22 21:15:34 +0200

    remove old code

--- src/pan_and_level.cpp
+++ src/pan_and_level.cpp
@@ -15,15 +15,6 @@ void init_pan_and_level(float startLevel[NR_OF_BANDS], float left_pan[NR_OF_BAND
        left_level[i] =  left_pan[i] * p;
        right_level[i] = right_pan[i] * p;
    }
-/*
-   for (int i = 0; i < NR_OF_BANDS; i++) {
-       left_pan[i] = INITIAL_PAN - INITIAL_PAN_OFFSET;
-       right_pan[i] = INITIAL_PAN + INITIAL_PAN_OFFSET;
-       float p = pow(10, startLevel[i] / 20);
-       left_level[i] =  left_pan[i] * p;
-       right_level[i] = right_pan[i] * p;
-   }
-*/
 }
 
 
